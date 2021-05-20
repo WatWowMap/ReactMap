6e6b0a16bc8dcad5ff6b495c295c8f63896dcddc
TurtIeSocks <58572875+TurtIeSocks@users.noreply.github.com>
2021-05-20 14:46:26 -0400

    Submission Cell Popups + Red Tiles
    - Probably going to have to clear local storage to get red border transitions to work

--- src/components/markers/placementCell.js
+++ src/components/markers/placementCell.js
@@ -1,7 +1,10 @@
+import { useStore } from '../../hooks/useStore'
+
 export default function placementStyle(cellBlocked) {
+  const { tileServers: { style } } = useStore(state => state.settings)
   return {
     fillColor: cellBlocked ? 'black' : 'green',
-    color: 'black',
+    color: style === 'dark' ? 'red' : 'black',
     opacity: 0.75,
     fillOpacity: cellBlocked ? 0.25 : 0,
     weight: 0.35,
--- src/components/markers/typeCell.js
+++ src/components/markers/typeCell.js
@@ -1,20 +1,26 @@
+import { useStore } from '../../hooks/useStore'
+
 export default function typeStyle(cell) {
+  const { tileServers: { style } } = useStore(state => state.settings)
+  const color = style === 'dark' ? 'red' : 'black'
   if ((cell.count === 1 && cell.count_gyms < 1)
   || (cell.count === 5 && cell.count_gyms < 2)
   || (cell.count === 19 && cell.count_gyms < 3)) {
     return {
-      fillColor: 'red', color: 'red', opacity: 0.75, fillOpacity: 0.5, weight: 0.75,
+      fillColor: 'red', color, opacity: 0.75, fillOpacity: 0.5, weight: 0.75,
     };
-  } if ((cell.count === 4 && cell.count_gyms < 2) || (cell.count === 18 && cell.count_gyms < 3)) {
+  }
+  if ((cell.count === 4 && cell.count_gyms < 2) || (cell.count === 18 && cell.count_gyms < 3)) {
     return {
-      fillColor: 'orange', color: 'red', opacity: 0.75, fillOpacity: 0.5, weight: 0.75,
+      fillColor: 'orange', color, opacity: 0.75, fillOpacity: 0.5, weight: 0.75,
     };
-  } if (cell.count >= 20) {
+  }
+  if (cell.count >= 20) {
     return {
-      fillColor: 'black', color: 'black', opacity: 0.75, fillOpacity: 0.25, weight: 0.8,
+      fillColor: 'black', color, opacity: 0.75, fillOpacity: 0.25, weight: 0.8,
     };
   }
   return {
-    fillColor: 'blue', color: 'black', opacity: 0.75, fillOpacity: 0.0, weight: 0.8,
+    fillColor: 'blue', color, opacity: 0.75, fillOpacity: 0.0, weight: 0.8,
   };
 }
