0f6dfad37706eb84524ce1534eb036b5d1521e60
Petap0w <cks85@msn.com>
2022-01-03 19:41:11 +0100

    Make profile (perms) optional

--- src/components/layout/Nav.jsx
+++ src/components/layout/Nav.jsx
@@ -33,7 +33,7 @@ export default function Nav({
   const { perms } = useStatic(state => state.auth)
   const webhookAlert = useStatic(state => state.webhookAlert)
   const setWebhookAlert = useStatic(state => state.setWebhookAlert)
-  const { map: { messageOfTheDay, donationPage } } = useStatic(state => state.config)
+  const { map: { enableUserProfile, messageOfTheDay, donationPage } } = useStatic(state => state.config)
   const userProfile = useStatic(state => state.userProfile)
   const setUserProfile = useStatic(state => state.setUserProfile)
   const feedback = useStatic(state => state.feedback)
@@ -123,7 +123,7 @@ export default function Nav({
           setDonorPage={setDonorPage}
         />
       )}
-      {userProfile ? (
+      {(enableUserProfile && userProfile) ? (
         <Dialog
           open={userProfile}
           fullScreen={isMobile}
