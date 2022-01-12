4a12e2af54a711676f36cd90803a2a7c4a39d472
TurtIeSocks <58572875+TurtIeSocks@users.noreply.github.com>
2022-01-11 19:00:23 -0500

    Zoom for Manual Areas

--- server/src/services/config.js
+++ server/src/services/config.js
@@ -56,6 +56,6 @@ config.scanAreas = fs.existsSync(`${__dirname}/../configs/areas.json`)
   ? require('../configs/areas.json')
   : { features: [] }
 
-config.manualAreas = Object.fromEntries(config.manualAreas.map(area => [area.name, { lat: area.lat, lon: area.lon }]))
+config.manualAreas = Object.fromEntries(config.manualAreas.map(area => [area.name, area]))
 
 module.exports = config
