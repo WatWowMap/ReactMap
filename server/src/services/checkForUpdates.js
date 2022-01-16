b566debd79d036d26c47a34b426e5401d441e25d
TurtIeSocks <58572875+TurtIeSocks@users.noreply.github.com>
2022-01-16 12:02:47 -0500

    Additional Version Error Checking
    - Checks if stdout does not return anything
    - Removes destructuring for latest json in case it fails

--- server/src/services/checkForUpdates.js
+++ server/src/services/checkForUpdates.js
@@ -6,17 +6,17 @@ const Fetch = require('./Fetch')
 
 exec('git branch --show-current', async (err, stdout) => {
   try {
-    if (err || typeof stdout !== 'string') {
+    if (err || typeof stdout !== 'string' || !stdout.trim()) {
       throw new Error('Unable to get current branch', err.message)
     }
-    const { version } = await Fetch.json(`https://raw.githubusercontent.com/WatWowMap/ReactMap/${stdout.trim()}/package.json`)
+    const latest = await Fetch.json(`https://raw.githubusercontent.com/WatWowMap/ReactMap/${stdout.trim()}/package.json`)
 
-    if (!version) {
+    if (!latest || !latest.version) {
       throw new Error('Unable to fetch latest version')
     }
 
     const [majorC, minorC, patchC] = currentVersion.split('.').map(x => parseInt(x))
-    const [majorN, minorN, patchN] = version.split('.').map(x => parseInt(x))
+    const [majorN, minorN, patchN] = latest.version.split('.').map(x => parseInt(x))
 
     if (stdout.trim() === 'main') {
       console.info('You are on the main branch, there may be additional features available on the "develop" branch. Type "git checkout develop" in the console if you would like the latest features.')
@@ -24,7 +24,7 @@ exec('git branch --show-current', async (err, stdout) => {
     if (majorC < majorN
       || (majorC === majorN && minorC < minorN)
       || (majorC === majorN && minorC === minorN && patchC < patchN)) {
-      console.info(`[${stdout.trim().toUpperCase()}] New version available: ${version} (Current: ${currentVersion})`)
+      console.info(`[${stdout.trim().toUpperCase()}] New version available: ${latest.version} (Current: ${currentVersion})`)
     }
   } catch (e) {
     console.error(e.message)
