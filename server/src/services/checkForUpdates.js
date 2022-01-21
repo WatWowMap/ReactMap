f6fbc2cb0188526712efe26a6a4f29f503daaf9b
TurtIeSocks <58572875+TurtIeSocks@users.noreply.github.com>
2022-01-21 17:48:38 -0500

    Possible docker fix

--- server/src/services/checkForUpdates.js
+++ server/src/services/checkForUpdates.js
@@ -4,36 +4,48 @@ const fs = require('fs')
 
 try {
   exec('git branch --show-current', async (err, stdout) => {
-    const gitRef = fs.readFileSync('.gitref', 'utf8')
+    try {
+      const gitRef = fs.readFileSync('.gitref', 'utf8')
 
-    if (!gitRef && (err || typeof stdout !== 'string' || !stdout.trim())) {
-      throw new Error('Unable to get current branch', err)
-    }
-    const branch = typeof gitRef === 'string' && gitRef.trim()
-      ? gitRef.trim()
-      : stdout.trim()
-
-    exec('git rev-parse HEAD', async (err2, stdout2) => {
-      const gitSha = fs.readFileSync('.gitsha', 'utf8')
-
-      if (!gitSha && (err2 || typeof stdout2 !== 'string' || !stdout2.trim())) {
-        throw new Error('Unable to get current sha', err)
+      if (!gitRef && (err || typeof stdout !== 'string' || !stdout.trim())) {
+        throw new Error('Unable to get current branch', err)
       }
-      const sha = typeof gitSha === 'string' && gitSha.trim()
-        ? gitSha.trim()
-        : stdout2.trim()
+      const branch = typeof gitRef === 'string' && gitRef.trim()
+        ? gitRef.split('/')[2].trim()
+        : stdout.trim()
 
-      exec(`git ls-remote https://github.com/WatWowMap/ReactMap/ refs/heads/${branch}`, (err3, stdout3) => {
-        if (err3 || typeof stdout3 !== 'string' || !stdout3?.split('\t')?.[0]) {
-          throw new Error('Unable to get remote sha', err3)
-        }
-        const remoteSha = stdout3.split('\t')[0]
+      exec('git rev-parse HEAD', async (err2, stdout2) => {
+        try {
+          const gitSha = fs.readFileSync('.gitsha', 'utf8')
 
-        if (remoteSha !== sha) {
-          console.log('There is a new version available:', remoteSha)
+          if (!gitSha && (err2 || typeof stdout2 !== 'string' || !stdout2.trim())) {
+            throw new Error('Unable to get current sha', err)
+          }
+          const sha = typeof gitSha === 'string' && gitSha.trim()
+            ? gitSha.trim()
+            : stdout2.trim()
+
+          exec(`git ls-remote https://github.com/WatWowMap/ReactMap/ refs/heads/${branch}`, (err3, stdout3) => {
+            try {
+              if (err3 || typeof stdout3 !== 'string' || !stdout3?.split('\t')?.[0]) {
+                throw new Error('Unable to get remote sha', err3)
+              }
+              const remoteSha = stdout3.split('\t')[0]
+
+              if (remoteSha !== sha) {
+                console.log('There is a new version available:', remoteSha)
+              }
+            } catch (e) {
+              console.warn('Unable to get remote SHA', e.message, 'Branch:', branch, 'Local SHA:', sha)
+            }
+          })
+        } catch (e) {
+          console.warn('Unable to get current SHA', e.message, 'Branch:', branch)
         }
       })
-    })
+    } catch (e) {
+      console.warn('Unable to get current branch', e.message)
+    }
   })
 } catch (e) {
   console.log(e.message)
