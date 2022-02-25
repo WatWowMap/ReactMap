fabddc0ddd59f3e0e28e418395fc1f4a0c06e252
TurtIeSocks <58572875+TurtIeSocks@users.noreply.github.com>
2022-02-24 22:13:30 -0500

    Fix Linting

--- src/components/tiles/Pokemon.jsx
+++ src/components/tiles/Pokemon.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-bitwise */
 import React, { memo, useRef, useState } from 'react'
 import { Marker, Popup, Circle } from 'react-leaflet'
 
@@ -19,14 +20,14 @@ const operator = {
 const cyrb53 = (str, seed = 0) => {
   let h1 = 0xdeadbeef ^ seed
   let h2 = 0x41c6ce57 ^ seed
-  for (let i = 0, ch; i < str.length; i++) {
-    ch = str.charCodeAt(i);
-    h1 = Math.imul(h1 ^ ch, 2654435761);
-    h2 = Math.imul(h2 ^ ch, 1597334677);
+  for (let i = 0, ch; i < str.length; i += 1) {
+    ch = str.charCodeAt(i)
+    h1 = Math.imul(h1 ^ ch, 2654435761)
+    h2 = Math.imul(h2 ^ ch, 1597334677)
   }
-  h1 = Math.imul(h1 ^ (h1>>>16), 2246822507) ^ Math.imul(h2 ^ (h2>>>13), 3266489909);
-  h2 = Math.imul(h2 ^ (h2>>>16), 2246822507) ^ Math.imul(h1 ^ (h1>>>13), 3266489909);
-  return [h1, h2];
+  h1 = Math.imul(h1 ^ (h1 >>> 16), 2246822507) ^ Math.imul(h2 ^ (h2 >>> 13), 3266489909)
+  h2 = Math.imul(h2 ^ (h2 >>> 16), 2246822507) ^ Math.imul(h1 ^ (h1 >>> 13), 3266489909)
+  return [h1, h2]
 }
 
 const getOffset = (coords, type, seed) => {
