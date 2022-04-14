312cefa59ed3f7cc753c8317f86849ae70da585f
TurtIeSocks <58572875+TurtIeSocks@users.noreply.github.com>
2022-04-14 09:10:58 -0400

    Update Config.jsx

--- src/components/Config.jsx
+++ src/components/Config.jsx
@@ -22,31 +22,18 @@ export default function Config() {
     }
   }
 
-  const getServerSettings = useCallback(async () => {
-    if (loadingText) {
-      loadingText.innerHTML = t('loading_settings')
+  if (rootLoading) {
+    if (serverSettings) {
+      rootLoading.style.display = 'none'
     }
+  }
+  const getServerSettings = useCallback(async () => {
     const data = await Fetch.getSettings()
     const Icons = data.masterfile ? new UIcons(data.config.icons, data.masterfile.questRewardTypes) : null
     if (Icons) {
-      if (loadingText) {
-        loadingText.innerText = t('loading_icons')
-      }
-      await Icons.fetchIcons(data.config.icons.styles)
+      Icons.build(data.config.icons.styles)
       if (data.config.icons.defaultIcons) {
         Icons.setSelection(data.config.icons.defaultIcons)
-      }
-    }
-    if (data.ui?.pokestops?.invasions && data.config?.map.fetchLatestInvasions) {
-      const invasionCache = JSON.parse(localStorage.getItem('invasions_cache'))
-      const cacheTime = data.config.map.invasionCacheHrs * 60 * 60 * 1000
-      if (invasionCache && invasionCache.lastFetched + cacheTime > Date.now()) {
-        data.masterfile.invasions = invasionCache
-      } else {
-        if (loadingText) {
-          loadingText.innerText = t('loading_invasions')
-        }
-        data.masterfile.invasions = await Fetch.getInvasions(data.masterfile.invasions)
       }
     }
     setServerSettings({ ...data, Icons })
@@ -54,6 +41,9 @@ export default function Config() {
 
   useEffect(() => {
     if (!serverSettings) {
+      if (loadingText) {
+        loadingText.innerText = t('loading_settings')
+      }
       getServerSettings()
     }
   }, [])
