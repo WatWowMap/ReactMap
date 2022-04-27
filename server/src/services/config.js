17fc0ac6a955c22a007aa530acb44999ebd4571d
TurtIeSocks <58572875+TurtIeSocks@users.noreply.github.com>
2022-04-26 20:04:32 -0400

    Docker Env Var Support
    - Add support for a variety of basic docker/docker-compose support

--- server/src/services/config.js
+++ server/src/services/config.js
@@ -9,9 +9,60 @@ const config = require('config')
 
 // Check if new config exists
 if (!fs.existsSync(path.resolve(`${__dirname}/../configs/local.json`))) {
-  console.log('[CONFIG] Config v2 (local.json) not found, you need to run the migration with "yarn config-migrate"')
-  process.exit(1)
+  // Only process env variables if local.json doesn't exist
+  // Generally for quick docker setups
+  const {
+    SCANNER_DB_HOST, SCANNER_DB_PORT, SCANNER_DB_NAME, SCANNER_DB_USERNAME, SCANNER_DB_PASSWORD,
+    MANUAL_DB_HOST, MANUAL_DB_PORT, MANUAL_DB_NAME, MANUAL_DB_USERNAME, MANUAL_DB_PASSWORD,
+    TITLE, START_LAT, START_LON,
+  } = process.env
+
+  if (SCANNER_DB_HOST && SCANNER_DB_PORT && SCANNER_DB_NAME && SCANNER_DB_USERNAME && SCANNER_DB_PASSWORD) {
+    config.database.schemas.push({
+      host: SCANNER_DB_HOST,
+      port: +SCANNER_DB_PORT,
+      database: SCANNER_DB_NAME,
+      username: SCANNER_DB_USERNAME,
+      password: SCANNER_DB_PASSWORD,
+      useFor: [
+        'device',
+        'gym',
+        'pokemon',
+        'pokestop',
+        'scanCell',
+        'spawnpoint',
+        'weather',
+      ],
+    })
+  } else {
+    throw new Error('Missing scanner database config! \nCheck to make sure you have SCANNER_DB_HOST,SCANNER_DB_PORT, SCANNER_DB_NAME, SCANNER_DB_USERNAME, and SCANNER_DB_PASSWORD')
+  }
+  if (MANUAL_DB_HOST && MANUAL_DB_PORT && MANUAL_DB_NAME && MANUAL_DB_USERNAME && MANUAL_DB_PASSWORD) {
+    config.database.schemas.push({
+      host: MANUAL_DB_HOST,
+      port: +MANUAL_DB_PORT,
+      database: MANUAL_DB_NAME,
+      username: MANUAL_DB_USERNAME,
+      password: MANUAL_DB_PASSWORD,
+      useFor: [
+        'session',
+        'user',
+        'nest',
+        'portal',
+      ],
+    })
+  } else {
+    throw new Error('Missing manual database config! \nCheck to make sure you have MANUAL_DB_HOST,MANUAL_DB_PORT, MANUAL_DB_NAME, MANUAL_DB_USERNAME, and MANUAL_DB_PASSWORD')
+  }
+  if (!TITLE || !START_LAT || !START_LON) {
+    console.warn('Missing, TITLE, START_LAT, OR START_LON\nYou will be able to process but but these are recommended to add to your docker-compose file')
+  }
+  config.map.general.title = TITLE
+  config.map.general.headerTitle = TITLE
+  config.map.general.startLat = +START_LAT
+  config.map.general.startLon = +START_LON
 }
+
 if (fs.existsSync(path.resolve(`${__dirname}/../configs/config.json`))) {
   console.log('[CONFIG] Config v1 (config.json) found, it is fine to leave it but make sure you are using and updating local.json instead.')
 }
