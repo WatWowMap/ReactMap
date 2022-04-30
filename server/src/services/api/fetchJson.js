1beb71132a03c8d737e9b0ee3c824a8ae681cf6b
TurtIeSocks <58572875+TurtIeSocks@users.noreply.github.com>
2022-04-29 23:45:15 -0400

    Add Fetch Timeout

--- server/src/services/api/fetchJson.js
+++ server/src/services/api/fetchJson.js
@@ -1,7 +1,15 @@
 /* eslint-disable no-console */
 const fetch = require('node-fetch')
+const { AbortError } = require('node-fetch')
+const { api } = require('../config')
 
 module.exports = async function fetchJson(url, options = undefined, log = false) {
+  const controller = new AbortController()
+
+  const timeout = setTimeout(() => {
+    controller.abort()
+  }, api.fetchTimeoutMs)
+
   try {
     if (log) console.log(url, options)
     const response = await fetch(url, options)
@@ -10,11 +18,15 @@ module.exports = async function fetchJson(url, options = undefined, log = false)
     }
     return response.json()
   } catch (e) {
-    if (log) {
+    if (e instanceof AbortError) {
+      console.log('Request to', url, 'timed out and was aborted')
+    } else if (log) {
       console.warn(e)
     } else {
       console.warn(e.message, '\n', e.code, `\nUnable to fetch ${url}`)
     }
     return null
+  } finally {
+    clearTimeout(timeout)
   }
 }
