47192a051cfdedff188ad17743f2258e23a9abdb
TurtIeSocks <58572875+TurtIeSocks@users.noreply.github.com>
2022-10-06 13:09:19 -0400

    consistency

--- server/src/routes/api/v1/users.js
+++ server/src/routes/api/v1/users.js
@@ -16,7 +16,7 @@ router.get('/', async (req, res) => {
     console.log('[API] api/v1/users')
   } catch (e) {
     console.error('[API Error] api/v1/sessions', e)
-    res.status(500).json({ status: 'ServerError', reason: e.message })
+    res.status(500).json({ status: 'error', reason: e.message })
   }
 })
 
@@ -27,14 +27,16 @@ router.get('/:id', async (req, res) => {
       req.headers['react-map-secret'] === api.reactMapSecret
     ) {
       const user = await User.query().findById(req.params.id)
-      res.status(200).json(user || { message: 'User not found' })
+      res
+        .status(200)
+        .json(user || { status: 'error', reason: 'User Not Found' })
     } else {
       throw new Error('Incorrect or missing API secret')
     }
     console.log(`[API] api/v1/users/${req.params.id}`)
   } catch (e) {
     console.error(`[API Error] api/v1/users/${req.params.id}`, e)
-    res.status(500).json({ status: 'ServerError', reason: e.message })
+    res.status(500).json({ status: 'error', reason: e.message })
   }
 })
 
@@ -45,14 +47,16 @@ router.get('/discord/:id', async (req, res) => {
       req.headers['react-map-secret'] === api.reactMapSecret
     ) {
       const user = await User.query().where('discordId', req.params.id).first()
-      res.status(200).json(user || { message: 'User not found' })
+      res
+        .status(200)
+        .json(user || { status: 'error', reason: 'User Not Found' })
     } else {
       throw new Error('Incorrect or missing API secret')
     }
     console.log(`[API] api/v1/users/discord/${req.params.id}`)
   } catch (e) {
     console.error(`[API Error] api/v1/users/discord/${req.params.id}`, e)
-    res.status(500).json({ status: 'ServerError', reason: e.message })
+    res.status(500).json({ status: 'error', reason: e.message })
   }
 })
 
@@ -63,14 +67,16 @@ router.get('/telegram/:id', async (req, res) => {
       req.headers['react-map-secret'] === api.reactMapSecret
     ) {
       const user = await User.query().where('telegramId', req.params.id).first()
-      res.status(200).json(user || { message: 'User not found' })
+      res
+        .status(200)
+        .json(user || { status: 'error', reason: 'User Not Found' })
     } else {
       throw new Error('Incorrect or missing API secret')
     }
     console.log(`[API] api/v1/users/telegram/${req.params.id}`)
   } catch (e) {
     console.error(`[API Error] api/v1/users/telegram/${req.params.id}`, e)
-    res.status(500).json({ status: 'ServerError', reason: e.message })
+    res.status(500).json({ status: 'error', reason: e.message })
   }
 })
 
