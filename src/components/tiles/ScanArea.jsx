86667a66dab9f3f4efd910454031dd588d4364a5
Derick M <58572875+TurtIeSocks@users.noreply.github.com>
2023-01-23 13:50:11 -0500

    shuffle async functions

--- src/components/tiles/ScanArea.jsx
+++ src/components/tiles/ScanArea.jsx
@@ -26,60 +26,59 @@ export function ScanAreaTile({
       setSelectedAreas([...selectedAreas, name])
     }
   }
-  const onEachFeature = (feature, layer) => {
-    if (feature.properties && feature.properties.name) {
-      const { name, key } = feature.properties
-      const popupContent = Utility.getProperName(name)
-      layer
-        .setStyle({
-          color:
-            feature.properties.color || feature.properties.stroke || '#3388ff',
-          weight: feature.properties['stroke-width'] || 3,
-          opacity: feature.properties['stroke-opacity'] || 1,
-          fillColor:
-            feature.properties.fillColor ||
-            feature.properties.fill ||
-            '#3388ff',
-          fillOpacity:
-            (selectedAreas?.includes(name?.toLowerCase()) &&
-              webhookMode === 'areas') ||
-            onlyAreas?.includes(webhookMode ? name : key)
-              ? 0.8
-              : 0.2,
-        })
-        .bindTooltip(popupContent, {
-          permanent: userSettings ? userSettings.alwaysShowLabels : true,
-          direction: 'top',
-          className: 'area-tooltip',
-        })
-      if (!userSettings || userSettings.alwaysShowLabels) {
-        layer.openTooltip()
-      }
-      if (webhookMode && name) {
-        layer.on('click', () => handleClick(name.toLowerCase()))
-      } else if (!feature.properties.manual && userSettings?.tapToToggle) {
-        layer.on('click', () => setAreas(key, names))
-      }
-    }
-  }
 
   return (
     <GeoJSON
       key={`${selectedAreas}-${onlyAreas ? onlyAreas.join('') : ''}-${
         filters?.filter?.search
       }`}
-      data={{
-        ...item,
-        features: item.features.filter(
-          (f) =>
-            webhookMode ||
-            filters?.filter?.search === '' ||
-            f.properties.key
-              .toLowerCase()
-              .includes(filters.filter.search.toLowerCase()),
-        ),
+      data={item}
+      filter={(f) =>
+        webhookMode ||
+        filters?.filter?.search === '' ||
+        f.properties.key
+          .toLowerCase()
+          .includes(filters.filter.search.toLowerCase())
+      }
+      pane="geojson"
+      onEachFeature={(feature, layer) => {
+        if (feature.properties && feature.properties.name) {
+          const { name, key } = feature.properties
+          const popupContent = Utility.getProperName(name)
+          layer
+            .setStyle({
+              color:
+                feature.properties.color ||
+                feature.properties.stroke ||
+                '#3388ff',
+              weight: feature.properties['stroke-width'] || 3,
+              opacity: feature.properties['stroke-opacity'] || 1,
+              fillColor:
+                feature.properties.fillColor ||
+                feature.properties.fill ||
+                '#3388ff',
+              fillOpacity:
+                (selectedAreas?.includes(name?.toLowerCase()) &&
+                  webhookMode === 'areas') ||
+                onlyAreas?.includes(webhookMode ? name : key)
+                  ? 0.8
+                  : 0.2,
+            })
+            .bindTooltip(popupContent, {
+              permanent: userSettings ? userSettings.alwaysShowLabels : true,
+              direction: 'top',
+              className: 'area-tooltip',
+            })
+          if (!userSettings || userSettings.alwaysShowLabels) {
+            layer.openTooltip()
+          }
+          if (webhookMode && name) {
+            layer.on('click', () => handleClick(name.toLowerCase()))
+          } else if (!feature.properties.manual && userSettings?.tapToToggle) {
+            layer.on('click', () => setAreas(key, names))
+          }
+        }
       }}
-      onEachFeature={onEachFeature}
     />
   )
 }
