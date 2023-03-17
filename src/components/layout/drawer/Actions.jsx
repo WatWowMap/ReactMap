f7a7c2241f7d084f1a11d984f0a89cf375ec8563
Derick M <58572875+TurtIeSocks@users.noreply.github.com>
2023-03-17 12:43:16 -0400

    rework drawer settings
    - rename "buttons" to "actions"
    - move from grid to list items
    - add icons to everything
    - add config option to have actions bunched in with settings (previously default) or on their own
    - add fallback url to uicons
    - some changes to how misc determines what's available

---
+++ src/components/layout/drawer/Actions.jsx
@@ -0,0 +1,146 @@
+import React, { forwardRef } from 'react'
+import {
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Link as MuiLink,
+} from '@material-ui/core'
+import { Link } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
+import AccountBoxIcon from '@material-ui/icons/AccountBox'
+import ExitToAppIcon from '@material-ui/icons/ExitToApp'
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline'
+import RotateLeftIcon from '@material-ui/icons/RotateLeft'
+import ImportExportIcon from '@material-ui/icons/ImportExport'
+import TrendingUpIcon from '@material-ui/icons/TrendingUp'
+import FeedbackIcon from '@material-ui/icons/Feedback'
+
+import { useStore, useStatic } from '@hooks/useStore'
+
+const importSettings = (e) => {
+  const file = e.target.files[0]
+  if (!file) {
+    return
+  }
+  const reader = new FileReader()
+  reader.onload = function parse(newSettings) {
+    const contents = newSettings.target.result
+    localStorage.clear()
+    localStorage.setItem('local-state', contents)
+  }
+  reader.readAsText(file)
+  setTimeout(() => window.location.reload(), 1500)
+}
+
+const exportSettings = () => {
+  const json = localStorage.getItem('local-state')
+  const el = document.createElement('a')
+  el.setAttribute(
+    'href',
+    `data:application/json;chartset=utf-8,${encodeURIComponent(json)}`,
+  )
+  el.setAttribute('download', 'settings.json')
+  el.style.display = 'none'
+  document.body.appendChild(el)
+  el.click()
+  document.body.removeChild(el)
+}
+
+const renderLink = forwardRef(({ to, ...itemProps }, ref) => (
+  <Link to={to} ref={ref} {...itemProps} />
+))
+
+export default function DrawerActions() {
+  const { t } = useTranslation()
+  const {
+    auth: { loggedIn, methods },
+    setUserProfile,
+    setFeedback,
+    setResetFilters,
+    config,
+  } = useStatic.getState()
+  const { setTutorial } = useStore.getState()
+
+  return (
+    <List>
+      {config.map.enableUserProfile && (
+        <ListItem button onClick={() => setUserProfile(true)}>
+          <ListItemIcon>
+            <AccountBoxIcon color="secondary" />
+          </ListItemIcon>
+          <ListItemText primary={t('profile')} />
+        </ListItem>
+      )}
+      {config.map.enableTutorial && (
+        <ListItem button onClick={() => setTutorial(true)}>
+          <ListItemIcon>
+            <HelpOutlineIcon color="secondary" />
+          </ListItemIcon>
+          <ListItemText primary={t('tutorial')} />
+        </ListItem>
+      )}
+      {config.map.statsLink && (
+        <ListItem
+          button
+          component="button"
+          href={config.map.statsLink}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <ListItemIcon>
+            <TrendingUpIcon color="action" />
+          </ListItemIcon>
+          <ListItemText primary={t('stats')} />
+        </ListItem>
+      )}
+      {config.map.feedbackLink && (
+        <ListItem button component="button" onClick={() => setFeedback(true)}>
+          <ListItemIcon>
+            <FeedbackIcon color="action" />
+          </ListItemIcon>
+          <ListItemText primary={t('feedback')} />
+        </ListItem>
+      )}
+      <input
+        accept="application/json"
+        id="contained-button-file"
+        type="file"
+        style={{ display: 'none' }}
+        onChange={importSettings}
+      />
+      <ListItem button component="label" htmlFor="contained-button-file">
+        <ListItemIcon>
+          <ImportExportIcon color="primary" />
+        </ListItemIcon>
+        <ListItemText primary={t('import')} />
+      </ListItem>
+      <ListItem button onClick={exportSettings}>
+        <ListItemIcon>
+          <ImportExportIcon color="secondary" />
+        </ListItemIcon>
+        <ListItemText primary={t('export')} />
+      </ListItem>
+      <ListItem button onClick={() => setResetFilters(true)}>
+        <ListItemIcon>
+          <RotateLeftIcon color="primary" />
+        </ListItemIcon>
+        <ListItemText primary={t('reset_filters')} />
+      </ListItem>
+      {!!methods.length && (
+        <ListItem
+          button
+          component={loggedIn ? MuiLink : renderLink}
+          to={loggedIn ? undefined : '/login'}
+          style={{ textDecoration: 'none', color: 'white' }}
+          href={loggedIn ? '/auth/logout' : undefined}
+        >
+          <ListItemIcon>
+            <ExitToAppIcon color="primary" />
+          </ListItemIcon>
+          <ListItemText primary={t(loggedIn ? 'logout' : 'login')} />
+        </ListItem>
+      )}
+    </List>
+  )
+}
--- src/components/layout/drawer/Settings.jsx
+++ src/components/layout/drawer/Settings.jsx
@@ -1,16 +1,53 @@
 import React from 'react'
 import shallow from 'zustand/shallow'
 import {
+  Divider,
   FormControl,
-  Grid,
   InputLabel,
+  List,
+  ListItem,
+  ListItemIcon,
   MenuItem,
   Select,
 } from '@material-ui/core'
+import TranslateIcon from '@material-ui/icons/Translate'
+import MapIcon from '@material-ui/icons/Map'
+import NavIcon from '@material-ui/icons/Navigation'
+import StyleIcon from '@material-ui/icons/Style'
+import DevicesOtherIcon from '@material-ui/icons/DevicesOther'
+
 import { useTranslation } from 'react-i18next'
 
 import { useStore, useStatic } from '@hooks/useStore'
 import Utility from '@services/Utility'
+import DrawerActions from './Actions'
+
+function FCSelect({ name, label, value, handleChange, children, icon }) {
+  return (
+    <ListItem dense>
+      {icon && <ListItemIcon>{icon}</ListItemIcon>}
+      <FormControl size="small" fullWidth style={{ margin: '3px 0' }}>
+        <InputLabel>{label}</InputLabel>
+        <Select
+          autoFocus
+          name={name}
+          value={value}
+          onChange={handleChange}
+          fullWidth
+        >
+          {children}
+        </Select>
+      </FormControl>
+    </ListItem>
+  )
+}
+
+const ICON_MAP = {
+  localeSelection: TranslateIcon,
+  navigation: NavIcon,
+  navigationControls: StyleIcon,
+  tileServers: MapIcon,
+}
 
 export default function Settings() {
   const { t, i18n } = useTranslation()
@@ -20,79 +57,69 @@ export default function Settings() {
   const { Icons, settings: staticSettings } = useStatic((s) => s, shallow)
   const { settings, icons } = useStore((state) => state, shallow)
 
-  const handleChange = (event) => {
-    setSettings({
-      ...settings,
-      [event.target.name]: config[event.target.name][event.target.value].name,
-    })
-    if (event.target.name === 'localeSelection') {
-      i18n.changeLanguage(event.target.value)
-    }
-  }
-
-  const handleIconChange = (event) => {
-    const { name, value } = event.target
-    Icons.setSelection(name, value)
-    setStaticIcons(Icons)
-    setIcons({ ...icons, [name]: value })
-  }
-
   return (
-    <Grid
-      container
-      direction="row"
-      justifyContent="space-evenly"
-      alignItems="center"
-      spacing={1}
-    >
-      {Object.keys(staticSettings).map((setting) => (
-        <Grid item key={setting} xs={10}>
-          <FormControl style={{ width: 200, margin: 5 }}>
-            <InputLabel>{t(Utility.camelToSnake(setting))}</InputLabel>
-            <Select
-              autoFocus
-              name={setting}
-              value={config[setting][settings[setting]]?.name || ''}
-              onChange={handleChange}
-              fullWidth
-            >
-              {Object.keys(config[setting]).map((option) => (
-                <MenuItem key={option} value={option}>
-                  {t(
-                    `${Utility.camelToSnake(setting)}_${option.toLowerCase()}`,
-                    Utility.getProperName(option),
-                  )}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-        </Grid>
-      ))}
+    <List dense style={{ width: '100%' }}>
+      {Object.keys(staticSettings).map((setting) => {
+        const Icon = ICON_MAP[setting] || DevicesOtherIcon
+
+        return (
+          <FCSelect
+            key={setting}
+            name={setting}
+            value={config[setting][settings[setting]]?.name || ''}
+            label={t(Utility.camelToSnake(setting))}
+            onChange={({ target }) => {
+              setSettings({
+                ...settings,
+                [target.name]: config[target.name][target.value].name,
+              })
+              if (target.name === 'localeSelection') {
+                i18n.changeLanguage(target.value)
+              }
+            }}
+            icon={<Icon style={{ color: 'white' }} />}
+          >
+            {Object.keys(config[setting]).map((option) => (
+              <MenuItem key={option} value={option}>
+                {t(
+                  `${Utility.camelToSnake(setting)}_${option.toLowerCase()}`,
+                  Utility.getProperName(option),
+                )}
+              </MenuItem>
+            ))}
+          </FCSelect>
+        )
+      })}
+      <Divider style={{ margin: '10px 0' }} />
       {Icons.customizable.map((category) => (
-        <Grid item key={category} xs={10}>
-          <FormControl style={{ width: 200, margin: 5 }}>
-            <InputLabel>
-              {t(`${category}_icons`, `${category} Icons`)}
-            </InputLabel>
-            <Select
-              autoFocus
-              name={category}
-              value={icons[category]}
-              onChange={handleIconChange}
-              fullWidth
-            >
-              {Icons[category].map((option) => (
-                <MenuItem key={option} value={option}>
-                  {t(
-                    `${category.toLowerCase()}_${option.toLowerCase()}`,
-                    Utility.getProperName(option),
-                  )}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-        </Grid>
+        <FCSelect
+          key={category}
+          name={category}
+          value={icons[category]}
+          label={t(`${category}_icons`, `${category} Icons`)}
+          handleChange={({ target }) => {
+            Icons.setSelection(target.name, target.value)
+            setStaticIcons(Icons)
+            setIcons({ ...icons, [target.name]: target.value })
+          }}
+          icon={<img src={Icons.getMisc(category)} alt={category} width={25} />}
+        >
+          {Icons[category].map((option) => (
+            <MenuItem key={option} value={option}>
+              {t(
+                `${category.toLowerCase()}_${option.toLowerCase()}`,
+                Utility.getProperName(option),
+              )}
+            </MenuItem>
+          ))}
+        </FCSelect>
       ))}
-    </Grid>
+      {!config.map?.separateDrawerActions && (
+        <>
+          <Divider style={{ margin: '10px 0' }} />
+          <DrawerActions />
+        </>
+      )}
+    </List>
   )
 }
