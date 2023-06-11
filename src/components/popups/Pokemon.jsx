bcdf8888f640aecafe6dd11d08e57f5e059f1871
ReuschelCGN <82573872+ReuschelCGN@users.noreply.github.com>
2023-06-11 20:58:11 +0200

    Update Pokemon.jsx

--- src/components/popups/Pokemon.jsx
+++ src/components/popups/Pokemon.jsx
@@ -42,6 +42,7 @@ export default function PokemonPopup({
   isTutorial,
   Icons,
   timeOfDay,
+  config,
 }) {
   const { t } = useTranslation()
   const classes = useStyles()
@@ -136,7 +137,13 @@ export default function PokemonPopup({
           ))}
         </Collapse>
         <Collapse in={popups.extras} timeout="auto" unmountOnExit>
-          <ExtraInfo pokemon={pokemon} perms={pokePerms} t={t} Icons={Icons} />
+          <ExtraInfo
+            pokemon={pokemon}
+            perms={pokePerms}
+            config={config}
+            t={t}
+            Icons={Icons}
+          />
         </Collapse>
       </Grid>
     </ErrorBoundary>
@@ -508,7 +515,7 @@ const Footer = ({ pokemon, popups, setPopups, hasPvp, classes, Icons }) => {
   )
 }
 
-const ExtraInfo = ({ pokemon, perms, t, Icons }) => {
+const ExtraInfo = ({ pokemon, perms, config, t, Icons }) => {
   const { moves } = useStatic((state) => state.masterfile)
 
   const { move_1, move_2, weight, height, first_seen_timestamp, updated, iv } =
@@ -582,6 +589,13 @@ const ExtraInfo = ({ pokemon, perms, t, Icons }) => {
         <Grid item xs={12} style={{ paddingTop: 10 }}>
           <Typography variant="subtitle1" align="center">
             {pokemon.id}
+          </Typography>
+        </Grid>
+      )}
+      {config.enablePokemonPopupCoords && (
+        <Grid item xs={12} style={{ textAlign: 'center' }}>
+          <Typography variant="caption" style={{ textAlign: 'center' }}>
+            🎯 {pokemon.lat.toFixed(6)}, {pokemon.lon.toFixed(6)}
           </Typography>
         </Grid>
       )}
