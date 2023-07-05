b5339b212a24587e2d8d2117a85f2b1774049f97
Derick M <58572875+TurtIeSocks@users.noreply.github.com>
2023-07-05 11:28:34 -0400

    fix: removeChild cleanup fn

--- src/components/layout/auth/Telegram.jsx
+++ src/components/layout/auth/Telegram.jsx
@@ -4,22 +4,26 @@ export default function Telegram({ botName, authUrl }) {
   const ref = useRef(null)
 
   useEffect(() => {
-    if (ref.current === null) return
+    if (ref.current) {
+      const script = document.createElement('script')
+      script.src = 'https://telegram.org/js/telegram-widget.js?4'
+      script.setAttribute('data-telegram-login', botName)
+      script.setAttribute('data-auth-url', authUrl)
+      script.setAttribute(
+        'data-lang',
+        localStorage?.getItem('i18nextLng') || 'en',
+      )
+      script.setAttribute('data-userpic', 'false')
+      script.setAttribute('data-size', 'large')
+      script.setAttribute('data-request-access', 'write')
+      script.async = true
 
-    const script = document.createElement('script')
-    script.src = 'https://telegram.org/js/telegram-widget.js?4'
-    script.setAttribute('data-telegram-login', botName)
-    script.setAttribute('data-auth-url', authUrl)
-    script.setAttribute(
-      'data-lang',
-      localStorage?.getItem('i18nextLng') || 'en',
-    )
-    script.setAttribute('data-userpic', 'false')
-    script.setAttribute('data-size', 'large')
-    script.setAttribute('data-request-access', 'write')
-    script.async = true
+      ref.current.appendChild(script)
 
-    ref.current.appendChild(script)
+      return () => {
+        ref.current.removeChild(script)
+      }
+    }
   }, [botName, authUrl, ref])
 
   return <div ref={ref} />
