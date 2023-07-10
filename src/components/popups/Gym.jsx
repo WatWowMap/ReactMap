e7238bcd0e044dbd8f7fabd94d1e59980fabc7f9
Derick M <58572875+TurtIeSocks@users.noreply.github.com>
2023-07-10 18:41:40 -0400

    feat: show badge

--- src/components/popups/Gym.jsx
+++ src/components/popups/Gym.jsx
@@ -289,7 +289,11 @@ const PoiImage = ({ gym, Icons }) => {
       <img
         src={src}
         alt={name || 'unknown'}
-        className={`circle-image team-${team_id}`}
+        className={`${
+          gym.badge
+            ? `badge badge-${gym.badge}`
+            : `circle-image team-${team_id}`
+        }`}
         style={{
           maxHeight: 75,
           maxWidth: 75,
@@ -382,6 +386,7 @@ const GymInfo = ({ gym, t, Icons }) => {
     ex_raid_eligible,
     ar_scan_eligible,
     updated,
+    badge,
   } = gym
   const { gymValidDataLimit } = useStatic((state) => state.config)
 
@@ -394,6 +399,13 @@ const GymInfo = ({ gym, t, Icons }) => {
       justifyContent="space-around"
       alignItems="center"
     >
+      {!!badge && (
+        <Grid item xs={12}>
+          <Typography variant="h6" align="center">
+            {t(`badge_${badge}`)}
+          </Typography>
+        </Grid>
+      )}
       {updated > gymValidDataLimit && (
           <Grid item xs={12}>
             <Typography variant="h6" align="center">
@@ -402,7 +414,7 @@ const GymInfo = ({ gym, t, Icons }) => {
           </Grid>
         ) && (
           <Grid item xs={12}>
-            <Typography variant="subtitle1" align="center">
+            <Typography variant="h6" align="center">
               {available_slots} {t('slots')}
             </Typography>
           </Grid>
@@ -410,25 +422,38 @@ const GymInfo = ({ gym, t, Icons }) => {
       {ex_raid_eligible && (
         <Grid
           item
-          xs={6}
+          xs={4}
           className="grid-item"
           style={{
             height: 24,
             backgroundImage: `url(${Icons.getMisc('ex')})`,
+            backgroundSize: 'contain',
           }}
         />
       )}
       {ar_scan_eligible && (
         <Grid
           item
-          xs={6}
+          xs={4}
           className="grid-item"
           style={{
             height: 24,
             backgroundImage: `url(${Icons.getMisc('ar')})`,
+            backgroundSize: 'contain',
           }}
         />
       )}
+      <Grid
+        item
+        xs={4}
+        style={{
+          background: `url(${Icons.getTeams(team_id)})`,
+          height: 24,
+          backgroundSize: 'contain',
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'center',
+        }}
+      />
     </Grid>
   )
 }
