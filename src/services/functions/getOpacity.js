96970570d3dccd27c73a085ad71793435ce3331c
Derick M <58572875+TurtIeSocks@users.noreply.github.com>
2023-07-19 19:26:35 -0400

    fix: add fallbacks

--- src/services/functions/getOpacity.js
+++ src/services/functions/getOpacity.js
@@ -8,7 +8,7 @@ export default function getOpacity(timestamp, userSettings) {
   const now = Math.floor(Date.now() / 1000)
   const diff = timestamp - now
   if (!diff || diff > 600) return 1
-  if (diff > 300) return userSettings.opacityTenMinutes
-  if (diff > 60) return userSettings.opacityFiveMinutes
-  return userSettings.opacityOneMinute
+  if (diff > 300) return userSettings.opacityTenMinutes || 0.75
+  if (diff > 60) return userSettings.opacityFiveMinutes || 0.5
+  return userSettings.opacityOneMinute || 0.25
 }
