8fcf3db74754ffa80b13d9a48e170fa453122a31
Derick M <58572875+TurtIeSocks@users.noreply.github.com>
2023-07-23 18:14:20 -0400

    styling: run prettier on migration files

--- server/src/db/migrations/20211119195323_local-auth.cjs
+++ server/src/db/migrations/20211119195323_local-auth.cjs
@@ -1,24 +1,28 @@
 /* eslint-disable no-unused-vars */
-const { database: { settings: { userTableName: tableName } } } = require('../../services/config')
+const {
+  database: {
+    settings: { userTableName: tableName },
+  },
+} = require('../../services/config')
 
 /**
- * @typedef {import("knex")} Knex
+ * @typedef {import("knex").Knex} Knex
  */
 
 /**
  * @param {Knex} knex
  */
-exports.up = async (knex) => knex.schema
-  .table(tableName, (table) => {
+exports.up = async (knex) =>
+  knex.schema.table(tableName, (table) => {
     table.string('username')
     table.string('password')
   })
 
 /**
-  * @param {Knex} knex
-  */
-exports.down = async (knex) => knex.schema
-  .table(tableName, (table) => {
+ * @param {Knex} knex
+ */
+exports.down = async (knex) =>
+  knex.schema.table(tableName, (table) => {
     table.dropColumn('username')
     table.dropColumn('password')
   })
