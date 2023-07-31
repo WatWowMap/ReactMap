a562d80e44d8c6588cdf7d03a7b2cafa40779135
lenisko <10072920+lenisko@users.noreply.github.com>
2023-08-01 01:34:39 +0200

    fix: lint

--- server/src/routes/authRouter.js
+++ server/src/routes/authRouter.js
@@ -13,15 +13,15 @@ strategies.forEach((strategy, i) => {
     strategy.type === 'discord' || strategy.type === 'telegram' ? 'get' : 'post'
   if (strategy.enabled) {
     const name = strategy.name ?? `${strategy.type}-${i}`
-    const callbackOptions = {};
+    const callbackOptions = {}
     const authenticateOptions = {
       failureRedirect: '/',
       successRedirect: '/',
-    };
-    if (strategy.type === 'discord') {
-      callbackOptions.prompt = strategy.clientPrompt;
     }
-    router[method](`/${name}`, passport.authenticate(name, authenticateOptions));
+    if (strategy.type === 'discord') {
+      callbackOptions.prompt = strategy.clientPrompt
+    }
+    router[method](`/${name}`, passport.authenticate(name, authenticateOptions))
     router[method](`/${name}/callback`, async (req, res, next) =>
       passport.authenticate(name, callbackOptions, async (err, user, info) => {
         if (err) {
