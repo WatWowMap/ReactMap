18ba8323533775e2ca0cedcd845491b09587515f
Derick M <58572875+TurtIeSocks@users.noreply.github.com>
2023-08-09 14:20:06 -0400

    feat: log fetch errors to file

--- server/src/services/api/fetchJson.js
+++ server/src/services/api/fetchJson.js
@@ -1,8 +1,17 @@
+const fs = require('fs')
+const { resolve } = require('path')
+
 const fetch = require('node-fetch')
 const config = require('../config')
 const { log, HELPERS } = require('../logger')
 
-module.exports = async function fetchJson(url, options = undefined) {
+/**
+ * fetch wrapper with timeout and error handling
+ * @param {string} url
+ * @param {RequestInit} [options]
+ * @returns
+ */
+async function fetchJson(url, options = undefined) {
   const controller = new AbortController()
 
   const timeout = setTimeout(() => {
@@ -18,8 +27,20 @@ module.exports = async function fetchJson(url, options = undefined) {
     return response.json()
   } catch (e) {
     log.warn(HELPERS.fetch, `Unable to fetch ${url}`, '\n', e)
+    if (options) {
+      fs.writeFileSync(
+        resolve(
+          __dirname,
+          '../../../../logs',
+          `${url.replaceAll('/', '_')}${Math.floor(Date.now() / 1000)}.json`,
+        ),
+        JSON.stringify(options, null, 2),
+      )
+    }
     return null
   } finally {
     clearTimeout(timeout)
   }
 }
+
+module.exports = fetchJson
