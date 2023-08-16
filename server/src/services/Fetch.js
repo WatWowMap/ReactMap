59d4fc2c161949f8ee82bbde65777c8422a17e74
Derick M <58572875+TurtIeSocks@users.noreply.github.com>
2023-08-15 20:49:33 -0400

    cleanup: remove old files

--- server/src/services/Fetch.js
+++ server/src/services/Fetch.js
@@ -1,14 +1,9 @@
 const fetchJson = require('./api/fetchJson')
-const webhookApi = require('./api/webhookApi')
 const scannerApi = require('./api/scannerApi')
 
 module.exports = class Fetch {
   static async json(url, options) {
     return fetchJson(url, options)
-  }
-
-  static async webhookApi(...args) {
-    return webhookApi(...args)
   }
 
   static async scannerApi(...args) {
