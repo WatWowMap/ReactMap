8689733b3b44c6e928fa11e6d68327760238367e
Derick M <58572875+TurtIeSocks@users.noreply.github.com>
2023-08-18 19:49:02 -0400

    documented `I` component

--- types/client.d.ts
+++ types/client.d.ts
@@ -1,5 +1,10 @@
+import * as React from 'react'
 import { Config } from './config'
 
 declare global {
   declare const CONFIG: Config
+}
+
+export interface CustomI extends React.HTMLProps<HTMLLIElement> {
+  size?: 'small' | 'medium'
 }
