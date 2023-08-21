9c4fddc0a8b8b771b3e4c4734005588f00d06290
Derick M <58572875+TurtIeSocks@users.noreply.github.com>
2023-08-21 15:16:22 -0400

    refactor: state of menu filters

--- src/components/layout/general/Menu.jsx
+++ src/components/layout/general/Menu.jsx
@@ -34,7 +34,6 @@ export default function Menu({
   const isTablet = useStatic((s) => s.isTablet)
 
   const menus = useStore((state) => state.menus)
-  const advMenu = useStore((state) => state.advMenu)
   const { t } = useTranslation()
   const Icons = useStatic((s) => s.Icons)
 
@@ -79,15 +78,18 @@ export default function Menu({
     setTempFilters({ ...tempFilters, ...newObj })
   }
 
-  const toggleDrawer = (open) => (event) => {
-    if (
-      event.type === 'keydown' &&
-      (event.key === 'Tab' || event.key === 'Shift')
-    ) {
-      return
-    }
-    setFilterDrawer(open)
-  }
+  const toggleDrawer = React.useCallback(
+    (open) => (event) => {
+      if (
+        event.type === 'keydown' &&
+        (event.key === 'Tab' || event.key === 'Shift')
+      ) {
+        return
+      }
+      setFilterDrawer(open)
+    },
+    [],
+  )
 
   const toggleAdvMenu = (open, id, newFilters) => (event) => {
     if (
@@ -199,72 +201,55 @@ export default function Menu({
     }
   }
 
-  const handleReset = () => {
-    const resetPayload = {}
-    Object.keys(menus[category].filters).forEach((cat) => {
-      resetPayload[cat] = {}
-      Object.keys(menus[category].filters[cat]).forEach((filter) => {
-        resetPayload[cat][filter] = false
-      })
-    })
-    useStore.setState((prev) => ({
-      menus: {
-        ...prev.menus,
-        [category]: { ...prev.menus[category], filters: resetPayload },
-      },
-    }))
-  }
-
-  const Options = (
-    <OptionsContainer
-      count={count}
-      category={category}
-      handleReset={handleReset}
-      advMenu={advMenu}
-      setAdvMenu={(value) => useStore.setState({ advMenu: value })}
-      search={search}
-      setSearch={setSearch}
-      menus={menus}
-      setMenus={(value) => useStore.setState({ menus: value })}
-      toggleDrawer={toggleDrawer}
-      isMobile={isMobile}
-      categories={categories}
-    />
+  const Options = React.useMemo(
+    () => (
+      <OptionsContainer
+        countTotal={count.total}
+        countShow={count.show}
+        category={category}
+        toggleDrawer={toggleDrawer}
+        categories={categories}
+      />
+    ),
+    [category, categories],
   )
 
-  const footerButtons = [
-    {
-      name: 'help',
-      action: () => setHelpDialog(!helpDialog),
-      icon: 'HelpOutline',
-    },
-    {
-      name: 'openFilter',
-      action: toggleDrawer(true),
-      icon: 'Ballot',
-      mobileOnly: true,
-    },
-    {
-      name: 'apply_to_all',
-      action: webhookCategory
-        ? toggleWebhook(true, 'global')
-        : toggleAdvMenu(true, 'global'),
-      icon: category === 'pokemon' || webhookCategory ? 'Tune' : 'FormatSize',
-    },
-    {
-      name: 'disable_all',
-      action: () => selectAllOrNone(false),
-      icon: 'Clear',
-      color: 'error',
-    },
-    {
-      name: 'enable_all',
-      action: () => selectAllOrNone(true),
-      icon: 'Check',
-      color: 'success',
-    },
-    ...extraButtons,
-  ]
+  const footerButtons = React.useMemo(
+    () => [
+      {
+        name: 'help',
+        action: () => setHelpDialog((prev) => !prev),
+        icon: 'HelpOutline',
+      },
+      {
+        name: 'openFilter',
+        action: toggleDrawer(true),
+        icon: 'Ballot',
+        mobileOnly: true,
+      },
+      {
+        name: 'apply_to_all',
+        action: webhookCategory
+          ? toggleWebhook(true, 'global')
+          : toggleAdvMenu(true, 'global'),
+        icon: category === 'pokemon' || webhookCategory ? 'Tune' : 'FormatSize',
+      },
+      {
+        name: 'disable_all',
+        action: () => selectAllOrNone(false),
+        icon: 'Clear',
+        color: 'error',
+      },
+      {
+        name: 'enable_all',
+        action: () => selectAllOrNone(true),
+        icon: 'Check',
+        color: 'success',
+      },
+      ...extraButtons,
+    ],
+    [category, webhookCategory, extraButtons],
+  )
 
   return (
     <>
