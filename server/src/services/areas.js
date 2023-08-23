efaf6d114e0b791d4c3c8ae2b6c1e2144bbf40f6
ReuschelCGN <82573872+ReuschelCGN@users.noreply.github.com>
2023-08-23 09:49:23 +0200

    Update areas.js

--- server/src/services/areas.js
+++ server/src/services/areas.js
@@ -247,7 +247,8 @@ const getAreas = async () => {
           .getSafe('multiDomains')
           .map(async (d) => [
             d.general?.geoJsonFileName
-            ? d.domain.replaceAll('.','_') : 'main',
+              ? d.domain.replaceAll('.','_')
+              : 'main',
             await loadScanPolygons(d.general?.geoJsonFileName || main),
           ]),
       ),
