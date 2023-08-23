e1c94d1dc2bc28ccd940cb66d5d4df11601e464b
Derick M <58572875+TurtIeSocks@users.noreply.github.com>
2023-08-23 11:16:32 -0400

    feat: put leaflet layers in their own components

--- src/hooks/useTileLayer.js
+++ src/hooks/useTileLayer.js
@@ -2,7 +2,7 @@ import * as React from 'react'
 
 import { useStatic, useStore } from '@hooks/useStore'
 
-const getTileServer = (tileServers, tileServer, timeOfDay) => {
+const getTileLayer = (tileServers, tileServer, timeOfDay) => {
   const fallbackTs = Object.values(tileServers).find(
     (server) => server.name !== 'auto',
   )
@@ -17,26 +17,28 @@ const getTileServer = (tileServers, tileServer, timeOfDay) => {
 }
 
 export default function useTileLayer() {
-  const { minZoom = 10, maxZoom = 18 } = useStatic.getState().config?.map || {}
-
-  const tileServers = useStatic((s) => s.config?.tileServers)
   const timeOfDay = useStatic((state) => state.timeOfDay)
   const userTileLayer = useStore((state) => state.settings.tileServers)
 
-  const tileServer = React.useMemo(
-    () => getTileServer(tileServers, userTileLayer, timeOfDay),
-    [timeOfDay, userTileLayer],
-  )
+  const tileLayer = React.useMemo(() => {
+    const { tileServers, map } = useStatic.getState().config || {
+      minZoom: 10,
+      maxZoom: 18,
+    }
+    const layer = getTileLayer(tileServers, userTileLayer, timeOfDay)
+    return {
+      ...layer,
+      style: layer.style || 'light',
+      key: `${layer?.name}_${timeOfDay}}`,
+      url:
+        layer?.[timeOfDay] ||
+        layer?.url ||
+        'https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png',
+      minZoom: map?.minZoom || 18,
+      maxZoom: map?.maxZoom || 10,
+      zIndex: 250,
+    }
+  }, [timeOfDay, userTileLayer])
 
-  return {
-    ...tileServer,
-    key: `${tileServer?.name}_${timeOfDay}}`,
-    url:
-      tileServer?.[timeOfDay] ||
-      tileServer?.url ||
-      'https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png',
-    minZoom,
-    maxZoom,
-    zIndex: 250,
-  }
+  return tileLayer
 }
