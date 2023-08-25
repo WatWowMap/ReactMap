ef18479bb2599cc8195b3bc80e8c42096a79b340
Derick M <58572875+TurtIeSocks@users.noreply.github.com>
2023-08-25 07:56:49 -0400

    fix: missing locales

--- src/features/playground/components/ToggleEditor.jsx
+++ src/features/playground/components/ToggleEditor.jsx
@@ -5,10 +5,12 @@ import ListItemIcon from '@mui/material/ListItemIcon'
 import ListItemText from '@mui/material/ListItemText'
 import Visibility from '@mui/icons-material/Visibility'
 import VisibilityOff from '@mui/icons-material/VisibilityOff'
+import { useTranslation } from 'react-i18next'
 
 import { toggleEditor, usePlayStore } from '../hooks/store'
 
 export function ToggleEditor() {
+  const { t } = useTranslation()
   const hideEditor = usePlayStore((s) => s.hideEditor)
 
   return (
@@ -16,7 +18,9 @@ export function ToggleEditor() {
       <ListItemIcon>
         {hideEditor ? <VisibilityOff /> : <Visibility />}
       </ListItemIcon>
-      <ListItemText>{hideEditor ? 'Show Editor' : 'Hide Editor'}</ListItemText>
+      <ListItemText>
+        {hideEditor ? t('show_editor') : t('hide_editor')}
+      </ListItemText>
     </MenuItem>
   )
 }
