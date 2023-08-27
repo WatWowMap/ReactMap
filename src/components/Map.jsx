117dafd393e66378bd52c4c749fa64fbcd514b83
Derick M <58572875+TurtIeSocks@users.noreply.github.com>
2023-08-27 16:54:14 -0400

    profiling logs only in dev mode

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -17,12 +17,13 @@ export default function Map() {
     <>
       {Object.keys({ ...ui, ...ui.wayfarer, ...ui.admin }).map((category) => {
         if (category === 'settings') return null
-        return (
+        return process.env.NODE_ENV === 'development' ? (
           <React.Profiler
             key={category}
             id={category}
             onRender={(id, phase, actualDuration, baseDuration) => {
               if (category === 'gyms')
+                // eslint-disable-next-line no-console
                 console.log(`[Profiler] ${id} (${phase})`, {
                   actualDuration,
                   baseDuration,
@@ -31,6 +32,8 @@ export default function Map() {
           >
             <FilterPermCheck key={category} category={category} />
           </React.Profiler>
+        ) : (
+          <FilterPermCheck key={category} category={category} />
         )
       })}
     </>
