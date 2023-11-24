bb9cbde0cb5505cbf813440ea7299db2cc08b1d8
Derick M <58572875+TurtIeSocks@users.noreply.github.com>
2023-11-24 17:14:10 -0500

    fix: maybe fix uicons stuff

--- src/hooks/useRefresh.js
+++ src/hooks/useRefresh.js
@@ -3,8 +3,8 @@ import { useEffect } from 'react'
 import { useQuery } from '@apollo/client'
 import getAvailable from '@services/queries/available'
 
-import UIcons from '@services/Icons'
 import { deepMerge } from '@services/functions/deepMerge'
+import UIcons from '@services/Icons'
 
 import { useStatic, useStore } from './useStore'
 
@@ -36,10 +36,11 @@ export default function useRefresh() {
     if (data?.available) {
       const { masterfile, filters, icons, ...rest } = data.available
       const { icons: userIcons } = useStore.getState()
-      const Icons = new UIcons(icons, masterfile.questRewardTypes)
+      const existing = useStatic.getState().Icons
+      const Icons = existing ?? new UIcons(icons, masterfile.questRewardTypes)
       if (Icons) {
         Icons.build(icons.styles)
-        if (icons.defaultIcons) {
+        if (icons.defaultIcons && !existing) {
           Icons.setSelection(icons.defaultIcons)
         }
         if (Icons.checkValid(userIcons)) {
