ced7d7c51401c96e2264e7028f83abe59f2c7fd7
Derick M <58572875+TurtIeSocks@users.noreply.github.com>
2024-01-16 23:51:00 -0500

    fix: mega energy icons by id

--- src/services/Icons.js
+++ src/services/Icons.js
@@ -264,7 +264,7 @@ class UAssets {
         return this.getPokestops(id.slice(1))
       case 'm':
         // mega energy
-        return this.getPokemon(id.slice(1).split('-')[0], 0, 1)
+        return this.getRewards(12, ...id.slice(1).split('-', 2))
       case 'p':
         // experience
         return this.getRewards(1, id.slice(1))
