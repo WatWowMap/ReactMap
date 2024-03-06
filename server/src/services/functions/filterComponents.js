72344a024fff3387fc22c5de9b2780d8264afd93
Derick M <58572875+TurtIeSocks@users.noreply.github.com>
2024-03-06 12:27:16 -0500

    fix: feedback from naji

--- server/src/services/functions/filterComponents.js
+++ server/src/services/functions/filterComponents.js
@@ -8,7 +8,7 @@
  */
 function filterComponents(components, loggedIn, donor) {
   return (Array.isArray(components) ? components : []).filter((component) => {
-    if ('components' in component) {
+    if ('components' in component && Array.isArray(component.components)) {
       return filterComponents(component.components, loggedIn, donor).length > 0
     }
     if (
