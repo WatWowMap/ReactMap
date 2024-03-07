bbe49e1dc45b93ba52d96c7cf8f70a1b088a27c1
Derick M <58572875+TurtIeSocks@users.noreply.github.com>
2024-03-07 14:20:21 -0500

    fix: area selected translation

--- src/features/webhooks/human/area/Selected.jsx
+++ src/features/webhooks/human/area/Selected.jsx
@@ -1,17 +1,16 @@
 // @ts-check
 import * as React from 'react'
 import Typography from '@mui/material/Typography'
-import { Trans } from 'react-i18next'
+import { useTranslation } from 'react-i18next'
 
 import { useWebhookStore } from '@store/useWebhookStore'
 
 export const Selected = () => {
+  const { t } = useTranslation()
   const selectedAreas = useWebhookStore((s) => s.human.area)
   return (
     <Typography variant="h6" align="center">
-      <Trans i18nKey="selected_areas" count={selectedAreas?.length || 0}>
-        {{ amount: selectedAreas?.length || 0 }}
-      </Trans>
+      {t('selected_areas', { count: selectedAreas?.length || 0 })}
     </Typography>
   )
 }
