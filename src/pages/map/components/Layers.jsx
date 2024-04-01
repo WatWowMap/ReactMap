704423b46cff627d902c2d2b905497f9bc941469
Derick M <58572875+TurtIeSocks@users.noreply.github.com>
2024-04-01 12:12:34 -0400

    fix: locate translations / unit of measurement

--- src/pages/map/components/Layers.jsx
+++ src/pages/map/components/Layers.jsx
@@ -2,6 +2,7 @@
 import * as React from 'react'
 import { TileLayer, useMap } from 'react-leaflet'
 import { control } from 'leaflet'
+import { useTranslation } from 'react-i18next'
 
 import { useStorage } from '@store/useStorage'
 
@@ -32,9 +33,11 @@ export function ControlledZoomLayer() {
 
 export function ControlledLocate() {
   const map = useMap()
+  const { t } = useTranslation()
   const navSetting = useStorage(
     (s) => s.settings.navigationControls === 'leaflet',
   )
+  const metric = useStorage((s) => s.settings.distanceUnit === 'kilometers')
 
   const lc = React.useMemo(
     () =>
@@ -43,14 +46,26 @@ export function ControlledLocate() {
         icon: 'fas fa-crosshairs',
         keepCurrentZoomLevel: true,
         setView: 'untilPan',
+        metric,
+        locateOptions: {
+          maximumAge: 5000,
+        },
+        strings: {
+          metersUnit: t('lc_metersUnit'),
+          feetUnit: t('lc_feetUnit'),
+          popup: t('lc_popup'),
+          outsideMapBoundsMsg: t('lc_outsideMapBoundsMsg'),
+          title: t('lc_title'),
+        },
       }),
-    [],
+    [t, metric],
   )
 
   React.useEffect(() => {
     if (navSetting) {
       lc.addTo(map)
       return () => {
+        lc.stop()
         lc.remove()
       }
     }
