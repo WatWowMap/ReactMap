ec35b141f0cf47bb66111c59c2d6e1ad52a2502a
Jeff <jeffc.bourassa@gmail.com>
2024-06-19 10:06:18 -0700

    run prettier on pokestop.js

--- server/src/models/Pokestop.js
+++ server/src/models/Pokestop.js
@@ -142,7 +142,11 @@ class Pokestop extends Model {
     const midnight = getUserMidnight(args)
     const ts = Math.floor(Date.now() / 1000)
 
-    const rocketGruntIDs = [4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,47,48,49,50]
+    const rocketGruntIDs = [
+      4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
+      24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 47, 48,
+      49, 50,
+    ]
     const rocketLeaderIDs = [41, 42, 43, 44]
 
     const {
@@ -581,14 +585,14 @@ class Pokestop extends Model {
               if (onlyExcludeGrunts) {
                 invasion.whereNotIn(
                   isMad ? 'character_display' : 'character',
-                  rocketGruntIDs
+                  rocketGruntIDs,
                 )
               }
-          
+
               if (onlyExcludeLeaders) {
                 invasion.whereNotIn(
                   isMad ? 'character_display' : 'character',
-                  rocketLeaderIDs
+                  rocketLeaderIDs,
                 )
               }
             })
@@ -613,14 +617,14 @@ class Pokestop extends Model {
               if (onlyExcludeGrunts) {
                 invasion.whereNotIn(
                   isMad ? 'character_display' : 'character',
-                  rocketGruntIDs
+                  rocketGruntIDs,
                 )
               }
-          
+
               if (onlyExcludeLeaders) {
                 invasion.whereNotIn(
                   isMad ? 'character_display' : 'character',
-                  rocketLeaderIDs
+                  rocketLeaderIDs,
                 )
               }
             })
