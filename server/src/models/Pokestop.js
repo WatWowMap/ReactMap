6e9adc0763a56b478fe0db09b249718257b681af
Jeff <jeffc.bourassa@gmail.com>
2024-06-19 09:34:37 -0700

    add back end support for the rocket filters

--- server/src/models/Pokestop.js
+++ server/src/models/Pokestop.js
@@ -135,10 +135,15 @@ class Pokestop extends Model {
         onlyEventStops,
         onlyConfirmed,
         onlyAreas = [],
+        onlyExcludeGrunts,
+        onlyExcludeLeaders,
       },
     } = args
     const midnight = getUserMidnight(args)
     const ts = Math.floor(Date.now() / 1000)
+
+    const rocketGruntIDs = [4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,47,48,49,50]
+    const rocketLeaderIDs = [41, 42, 43, 44]
 
     const {
       lures: lurePerms,
@@ -573,6 +578,13 @@ class Pokestop extends Model {
                   )
                 }
               })
+              if (onlyExcludeGrunts) {
+                invasion.whereNotIn('character', rocketGruntIDs)
+              }
+          
+              if (onlyExcludeLeaders) {
+                invasion.whereNotIn('character', rocketLeaderIDs)
+              }
             })
           } else {
             stops.orWhere((invasion) => {
@@ -591,6 +603,13 @@ class Pokestop extends Model {
               }
               if (hasConfirmed) {
                 invasion.andWhere('confirmed', onlyConfirmed)
+              }
+              if (onlyExcludeGrunts) {
+                invasion.whereNotIn('character', rocketGruntIDs)
+              }
+          
+              if (onlyExcludeLeaders) {
+                invasion.whereNotIn('character', rocketLeaderIDs)
               }
             })
           }
