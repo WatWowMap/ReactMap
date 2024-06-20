8f851babf6c7448deb4583c233a614bcac84a306
Jeff <jeffc.bourassa@gmail.com>
2024-06-19 18:58:00 -0700

    Dynamically get grunt IDs from invasions

--- server/src/models/Pokestop.js
+++ server/src/models/Pokestop.js
@@ -142,13 +142,6 @@ class Pokestop extends Model {
     const midnight = getUserMidnight(args)
     const ts = Math.floor(Date.now() / 1000)
 
-    const rocketGruntIDs = [
-      4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
-      24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 47, 48,
-      49, 50,
-    ]
-    const rocketLeaderIDs = [41, 42, 43, 44]
-
     const {
       lures: lurePerms,
       quests: questPerms,
@@ -585,14 +578,14 @@ class Pokestop extends Model {
               if (onlyExcludeGrunts) {
                 invasion.whereNotIn(
                   isMad ? 'character_display' : 'character',
-                  rocketGruntIDs,
+                  Event.rocketGruntIDs,
                 )
               }
 
               if (onlyExcludeLeaders) {
                 invasion.whereNotIn(
                   isMad ? 'character_display' : 'character',
-                  rocketLeaderIDs,
+                  Event.rocketLeaderIDs,
                 )
               }
             })
@@ -613,19 +606,6 @@ class Pokestop extends Model {
               }
               if (hasConfirmed) {
                 invasion.andWhere('confirmed', onlyConfirmed)
-              }
-              if (onlyExcludeGrunts) {
-                invasion.whereNotIn(
-                  isMad ? 'character_display' : 'character',
-                  rocketGruntIDs,
-                )
-              }
-
-              if (onlyExcludeLeaders) {
-                invasion.whereNotIn(
-                  isMad ? 'character_display' : 'character',
-                  rocketLeaderIDs,
-                )
               }
             })
           }
