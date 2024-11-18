38b563a973d3a4a5b78991bbfa98f23ecf3804de
Mygod <contact-git@mygod.be>
2024-11-18 14:17:14 -0800

    fix: show battle bonus when inactive

--- src/features/station/StationPopup.jsx
+++ src/features/station/StationPopup.jsx
@@ -57,8 +57,7 @@ export function StationPopup(station) {
         <StationRating {...station} />
       )}
       <StationMedia {...station} />
-      {!!station.is_battle_available &&
-        station.battle_start < Date.now() / 1000 &&
+      {station.battle_start < Date.now() / 1000 &&
         station.battle_end > Date.now() / 1000 && (
           <ExpandCollapse>
             <StationAttackBonus {...station} />
