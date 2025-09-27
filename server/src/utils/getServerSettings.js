150529035cf456275e3526bfe62350fa4ce06017
kamieniarz <9063907+kamieniarz@users.noreply.github.com>
2025-09-27 17:38:00 +0200

    Update getServerSettings.js

--- server/src/utils/getServerSettings.js
+++ server/src/utils/getServerSettings.js
@@ -1,5 +1,6 @@
 // @ts-check
 const config = require('@rm/config')
+const { knex } = require('knex')
 
 const { clientOptions } = require('../ui/clientOptions')
 const { advMenus } = require('../ui/advMenus')
@@ -9,13 +10,61 @@ const { drawer } = require('../ui/drawer')
  *
  * @param {import("express").Request} req
  */
-function getServerSettings(req) {
+let accessDb
+
+async function getServerSettings(req) {
   const user =
     /** @type {import('@rm/types').ExpressUser & { loggedIn: boolean; cooldown: number }} */ ({
       ...(req.user ? req.user : req.session),
       loggedIn: !!req.user,
       cooldown: req.session?.cooldown || 0,
     })
+
+  // Optional: fetch accessType and accessExpiration from external access DB
+  try {
+    const accessCfg = config.getSafe('accessDatabase')
+    if (accessCfg && accessCfg.host && accessCfg.database && accessCfg.table) {
+      if (!accessDb) {
+        accessDb = knex({
+          client: 'mysql2',
+          connection: {
+            host: accessCfg.host,
+            port: accessCfg.port || 3306,
+            user: accessCfg.username,
+            password: accessCfg.password,
+            database: accessCfg.database,
+          },
+          pool: { min: 0, max: 5 },
+        })
+      }
+      const matchBy = accessCfg.matchBy || 'discordId'
+      const matchColumn = accessCfg.matchColumn || (matchBy === 'username' ? 'username' : 'discord_id')
+      const typeColumn = accessCfg.typeColumn || 'access_type'
+      const expirationColumn = accessCfg.expirationColumn || 'access_expiration'
+
+      // Determine identifier to match on
+      const identifier = matchBy === 'username' ? user.username : user.discordId
+      if (identifier) {
+        // eslint-disable-next-line no-underscore-dangle
+        const row = await accessDb(accessCfg.table)
+          .select([typeColumn, expirationColumn])
+          .where(matchColumn, identifier)
+          .first()
+        user.accessType = row?.[typeColumn] ?? null
+        user.accessExpiration = row?.[expirationColumn] ?? null
+      } else {
+        user.accessType = null
+        user.accessExpiration = null
+      }
+    } else {
+      user.accessType = null
+      user.accessExpiration = null
+    }
+  } catch (e) {
+    // Fail safe: do not break settings payload if access DB lookup fails
+    user.accessType = null
+    user.accessExpiration = null
+  }
 
   const { clientValues, clientMenus } = clientOptions(user.perms)
 
