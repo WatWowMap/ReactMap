cb3ff095fb312133579a87884d391d2a4ee0afe7
Mygod <contact-git@mygod.be>
2025-11-23 23:15:18 -0800

    fix: stationed pokemon not updating in popup

--- src/features/station/useGetStationMons.js
+++ src/features/station/useGetStationMons.js
@@ -1,17 +1,33 @@
 // @ts-check
-import { useMemo } from 'react'
+import { useEffect, useMemo, useRef } from 'react'
 import { useQuery } from '@apollo/client'
 
 import { GET_STATION_POKEMON } from '@services/queries/station'
 
 /**
  * @param {string} id
+ * @param {number} [lastUpdated]
  * @returns {import('@rm/types').StationPokemon[]}
  */
-export function useGetStationMons(id) {
-  const { data } = useQuery(GET_STATION_POKEMON, {
+export function useGetStationMons(id, lastUpdated) {
+  const { data, refetch } = useQuery(GET_STATION_POKEMON, {
     variables: { id },
   })
+  const previousUpdated = useRef(lastUpdated)
+
+  useEffect(() => {
+    if (!Number.isFinite(lastUpdated)) {
+      previousUpdated.current = lastUpdated
+      return
+    }
+    if (
+      previousUpdated.current !== undefined &&
+      previousUpdated.current !== lastUpdated
+    ) {
+      refetch({ id })
+    }
+    previousUpdated.current = lastUpdated
+  }, [id, lastUpdated, refetch])
 
   return useMemo(() => data?.stationPokemon || [], [data])
 }
