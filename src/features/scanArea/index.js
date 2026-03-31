f7c97c1171eba271603665f1c1c3e22b94a89fc1
Mygod <contact-git@mygod.be>
2026-03-31 02:15:44 -0400

    fix(scanArea): gate legacy filter migration

--- src/features/scanArea/index.js
+++ src/features/scanArea/index.js
@@ -1,3 +1,4 @@
 // @ts-check
 
+export * from './LegacyMigration'
 export * from './ScanAreaTile'
--- src/pages/map/components/Container.jsx
+++ src/pages/map/components/Container.jsx
@@ -5,6 +5,7 @@ import { MapContainer } from 'react-leaflet'
 import { useMemory } from '@store/useMemory'
 import { useStorage } from '@store/useStorage'
 import { useMapStore } from '@store/useMapStore'
+import { LegacyScanAreaGate } from '@features/scanArea'
 import { ScanOnDemand } from '@features/scanner'
 import { WebhookMarker, WebhookAreaSelection } from '@features/webhooks'
 import { ActiveWeather } from '@features/weather'
@@ -39,34 +40,36 @@ export function Container() {
   const { location, zoom } = useStorage.getState()
 
   return (
-    <MapContainer
-      tap={false}
-      center={location}
-      ref={(ref) => {
-        if (ref) {
-          const { attributionPrefix } = useMemory.getState().config.general
-          ref.attributionControl.setPrefix(attributionPrefix || '')
-          ref.on('moveend', setLocationZoom)
-          ref.on('zoomend', setLocationZoom)
-        }
-        useMapStore.setState({ map: ref })
-      }}
-      zoom={zoom}
-      zoomControl={false}
-      maxBounds={MAX_BOUNDS}
-      preferCanvas
-    >
-      <Effects />
-      <ControlledTileLayer />
-      <ControlledZoomLayer />
-      <ControlledLocate />
-      <DataView />
-      <ScanOnDemand mode="scanNext" />
-      <ScanOnDemand mode="scanZone" />
-      <WebhookMarker />
-      <WebhookAreaSelection />
-      <Nav />
-      <ActiveWeather />
-    </MapContainer>
+    <LegacyScanAreaGate>
+      <MapContainer
+        tap={false}
+        center={location}
+        ref={(ref) => {
+          if (ref) {
+            const { attributionPrefix } = useMemory.getState().config.general
+            ref.attributionControl.setPrefix(attributionPrefix || '')
+            ref.on('moveend', setLocationZoom)
+            ref.on('zoomend', setLocationZoom)
+          }
+          useMapStore.setState({ map: ref })
+        }}
+        zoom={zoom}
+        zoomControl={false}
+        maxBounds={MAX_BOUNDS}
+        preferCanvas
+      >
+        <Effects />
+        <ControlledTileLayer />
+        <ControlledZoomLayer />
+        <ControlledLocate />
+        <DataView />
+        <ScanOnDemand mode="scanNext" />
+        <ScanOnDemand mode="scanZone" />
+        <WebhookMarker />
+        <WebhookAreaSelection />
+        <Nav />
+        <ActiveWeather />
+      </MapContainer>
+    </LegacyScanAreaGate>
   )
 }
