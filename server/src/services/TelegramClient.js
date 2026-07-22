57724699bb3b2d0a4f2aff09c0bda8a594f8303b
Mygod <contact-git@mygod.be>
2026-07-22 16:31:18 -0400

    fix: display-name fallback

--- server/src/services/TelegramClient.js
+++ server/src/services/TelegramClient.js
@@ -10,6 +10,7 @@ const { areaPerms } = require('../utils/areaPerms')
 const { webhookPerms } = require('../utils/webhookPerms')
 const { scannerPerms, scannerCooldownBypass } = require('../utils/scannerPerms')
 const { mergePerms } = require('../utils/mergePerms')
+const { getUserDisplayName } = require('../utils/getUserDisplayName')
 const { AuthClient } = require('./AuthClient')
 
 /**
@@ -122,7 +123,11 @@ class TelegramClient extends AuthClient {
 
   /** @type {import('@rainb0w-clwn/passport-telegram-official/dist/types').CallbackWithRequest} */
   async authHandler(req, profile, done) {
-    const baseUser = { ...profile, rmStrategy: this.rmStrategy }
+    const baseUser = {
+      ...profile,
+      username: getUserDisplayName(profile),
+      rmStrategy: this.rmStrategy,
+    }
     const groups = await this.getUserGroups(baseUser)
     const user = this.getUserPerms(baseUser, groups)
 
